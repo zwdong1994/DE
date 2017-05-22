0573c217b53d405f3546bfda7aee976e570e8a3a
victor <zwdong1994@gmail.com>
2017-05-22 16:56:24 +0800

    create this project

---
+++ cmake-build-debug/CMakeFiles/ed.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ed.dir/main.cpp.o"
+  "ed.pdb"
+  "ed"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
