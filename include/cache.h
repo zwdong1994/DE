55972475af6cca89b0844f6906465ee38b5584ab
victor <zwdong1994@gmail.com>
2017-05-24 20:59:30 +0800

    build cache modules but not finish

---
+++ include/cache.h
@@ -0,0 +1,14 @@
+//
+// Created by victor on 5/24/17.
+//
+
+#ifndef ED_CACHE_H
+#define ED_CACHE_H
+
+class cache{
+
+}
+
+
+#endif //ED_CACHE_H
+
---
+++ src/cache.cpp
@@ -0,0 +1,8 @@
+//
+// Created by victor on 5/24/17.
+//
+
+#include <string.h>
+#include <iostream>
+#include <map>
+
