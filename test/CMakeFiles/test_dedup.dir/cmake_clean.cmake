05e179bdb8b549bc9c75611724a8b60d43e1d46e
victor <zwdong1994@gmail.com>
2017-06-15 22:24:59 +0800

    Mid-trans-station version.

---
+++ test/CMakeFiles/test_dedup.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_dedup.dir/test_dedup.cpp.o"
+  "CMakeFiles/test_dedup.dir/home/victor/zwd/ecc_dedup/ed/src/bloom_func.cpp.o"
+  "CMakeFiles/test_dedup.dir/home/victor/zwd/ecc_dedup/ed/src/cache.cpp.o"
+  "CMakeFiles/test_dedup.dir/home/victor/zwd/ecc_dedup/ed/src/com_t.cpp.o"
+  "CMakeFiles/test_dedup.dir/home/victor/zwd/ecc_dedup/ed/src/mt.cpp.o"
+  "CMakeFiles/test_dedup.dir/home/victor/zwd/ecc_dedup/ed/src/dedup.cpp.o"
+  "CMakeFiles/test_dedup.dir/home/victor/zwd/ecc_dedup/ed/src/bch.c.o"
+  "test_dedup.pdb"
+  "test_dedup"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test_dedup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
