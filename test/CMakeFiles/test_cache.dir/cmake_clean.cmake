4625e79ac0ec8767ffbf57994adf53d2431270e1
victor <zwdong1994@gmail.com>
2017-06-18 20:59:46 +0800

    MD5 version

---
+++ test/CMakeFiles/test_cache.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cache.dir/test_cache.cpp.o"
+  "CMakeFiles/test_cache.dir/home/victor/zwd/ecc_dedup/DE/src/bloom_func.cpp.o"
+  "CMakeFiles/test_cache.dir/home/victor/zwd/ecc_dedup/DE/src/cache.cpp.o"
+  "CMakeFiles/test_cache.dir/home/victor/zwd/ecc_dedup/DE/src/com_t.cpp.o"
+  "CMakeFiles/test_cache.dir/home/victor/zwd/ecc_dedup/DE/src/mt.cpp.o"
+  "CMakeFiles/test_cache.dir/home/victor/zwd/ecc_dedup/DE/src/dedup.cpp.o"
+  "CMakeFiles/test_cache.dir/home/victor/zwd/ecc_dedup/DE/src/bch.c.o"
+  "test_cache.pdb"
+  "test_cache"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test_cache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
